7725f618ede907428b9622804754f69f60cadaba
ahmed0magdy <110637215+ahmed0magdy@users.noreply.github.com>
2022-11-15 15:46:09 +0200

    serverless last

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'prprcux5na';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/prod`
+
+export const authConfig = {
+  domain: 'dev-y4wytjgr4ohogutq.us.auth0.com',
+  clientId: 'BtJkIMhUnwckrlUND8Ae42sVTiQpOYv5',
+  callbackUrl: 'http://localhost:3000/callback'
+};
